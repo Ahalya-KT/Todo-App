f6b90784cd7660633d20e4f3c2704573c29993f0
Ahalya-KT <ahalyakt002@gmail.com>
2024-04-18 12:32:31 +0530

    todo-app

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,31 @@
 import Filter from "./components/Filter";
 import Task from "./components/Task";
 import "./App.css";
+import { useEffect, useState } from "react";
+
+
+const API_BASE = "http://https://todo-backend-n34f.onrender.com/todo";
 
 function App() {
+  // Add useState,we ll store items in the array
+  const [items, setItems] = useState([]);
+
+  // Add useEffect,GetTodos() will run every time the component renders
+  useEffect(() => {
+    GetTodos();
+  }, []);
+
+  // Add GetTodos() function, fetches data from our API, converts to JSON
+  // and then saves the data in the "items" state
+  // if there's an error, it will be logged to the console
+
+  const GetTodos = () => {
+    fetch(API_BASE)
+      .then((res) => res.json())
+      .then((data) => setItems(data))
+      .catch((err) => console.log(err));
+  };
+
   return (
     <div className="bg-gray-800 h-screen px-28 py-28">
       <h1 className="text-white text-2xl font-bold">
@@ -14,7 +37,7 @@ function App() {
           <Filter />
         </div>
         <div className="w-3/4 py-5">
-          <Task/>
+          <Task items={items} GetTodos={GetTodos} setItems={setItems}/>
         </div>
       </div>
     </div>
--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,60 +1,103 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import { AiFillDelete } from "react-icons/ai";
 
+const API_BASE = "http://https://todo-backend-n34f.onrender.com/todo";
 
-const Task = () => {
-  const [todo,setTodo]=useState("");
-  const[todoApp,setTodoApp]=useState([])
+// to post an  task
+const Task = ({ items, GetTodos,setItems }) => {
+  const addItem = async () => {
+    const data = await fetch(API_BASE + "/new", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        name: input,
+      }),
+    }).then((res) => res.json());
+    await GetTodos();
+    setInput("");
+  };
+  
+  // to delete a task
+  const deleteTodo = async(id) => {
+    try{
+        const response = await fetch(API_BASE + "/delete/" + id, {
+            method: "DELETE",
+          });
+        if(!response.ok){
+            throw new Error("Failed to delete a task")
+        } 
+        const data = await response.json()
+        setItems(items=> items.filter(item=> item._id !== data._id))
+    }catch (error) {
+        console.error("Error updating task status:", error);
+      }
+  }
 
-   const addTodo=()=>{
-    if(todo!==""){
-      setTodoApp([...todoApp,todo])
-      setTodo("")
-    }
-    
-   }
-   const deleteTodo=(text)=>{
-    const NewTodo=todoApp.filter((todo)=>{
-      return todo !==text;
-    })
-    setTodoApp(NewTodo);
-   }
+
+  // Add input state,we will store the users input in this state
+  const [input, setInput] = useState("");
+  // store the targets value into the input state
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  console.log(input, "input");
+  console.log(items, "error");
+
   return (
-        <div className='text-white bg-gray-950 p-11 rounded-lg'>
-      <div className='flex gap-6'>
-      <input type='text'
-      placeholder='Enter your Task'
-       className='bg-gray-800 p-3 rounded-lg w-full '
-       name='todo'
-       value={todo}
-       onChange={(e)=>{setTodo(e.target.value)}}
-
-       />
-      <button className='text-xs bg-blue-600 p-2 rounded-md whitespace-nowrap ' onClick={addTodo}>Add Task</button>
+    <div className="text-white bg-gray-950 p-11 rounded-lg">
+      <div className="flex gap-6">
+        <input
+          type="text"
+          placeholder="Enter your Task"
+          className="bg-gray-800 p-3 rounded-lg w-full"
+          name="todo"
+          value={input}
+          // onChange={(e) => {
+          //   setTodo(e.target.value);
+          // }}
+          onChange={handleChange}
+        />
+        <button
+          className="text-xs bg-blue-600 p-2 rounded-md whitespace-nowrap"
+          // onClick={addTodo}
+          onClick={() => addItem()}
+        >
+          Add Task
+        </button>
       </div>
 
-      <p className='py-4 text-blue-500 text-xl font-bold'>2 task <span className='text-white '>to</span>do</p>
-
-        {todoApp?.length>0?(
-             <div className='flex justify-between bg-gray-800 p-3 rounded-lg'>
-             {todoApp.map((todo, index) => (
-               <div key={index} className='flex items-center'>
-                 <p className='text-blue-500'>{todo}</p>
-                 <div className='text-blue-500 cursor-pointer'>
-                   <AiFillDelete color='blue' onClick={()=>{
-                     deleteTodo(todo)
-                   }} />
-                 </div>  
-               </div>
-             ))}
-           </div>
-        ):(
-          <div>
-            <p>No task found</p>
-          </div>
-        )} 
+      <p className="py-4 text-blue-500 text-xl font-bold">
+        2 task <span className="text-white ">to</span>do
+      </p>
+      {items?.length > 0 ? (
+        <div className="">
+          {items.map((item, index) => (
+            <div
+              key={index}
+              className="flex mb-3 justify-between p-3 items-center bg-gray-800 rounded-md"
+            >
+              <p className="text-blue-500">{item.name}</p>
+              <div className="text-blue-500 cursor-pointer">
+                <AiFillDelete
+                  color="blue"
+                  onClick={() => {
+                    deleteTodo(item._id);
+                  }}
+                />
+              </div>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <div>
+          <p>No task found</p>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Task
+export default Task;
